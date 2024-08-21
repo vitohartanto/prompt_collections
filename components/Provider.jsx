942848e77d31b0eb3aa55170dfaa78fcb86bd68f
Vito <vitohartanto@mail.ugm.ac.id>
2024-08-21 18:01:15 +0700

    fix: add use client to the provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SessionProvider } from 'next-auth/react';
 const Provider = ({ children, session }) => {
   return <SessionProvider session={session}>{children}</SessionProvider>;
